5e91ff5f19b2014729561c926aecede2b7fa44e3
bilmuh <bilmuh@bilmuh-pc>
2017-08-10 12:12:17 +0300

    actor, director ve movie ler çekildi

--- trex/src/main/java/main/QueryManager.java
+++ trex/src/main/java/main/QueryManager.java
@@ -4,14 +4,16 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import org.apache.jena.atlas.web.auth.HttpAuthenticator;
+import org.apache.jena.riot.RDFLanguages;
+
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.Model;
-import com.hp.hpl.jena.sparql.resultset.RDFOutput;
+import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
 
 public class QueryManager {
 
@@ -27,13 +29,25 @@ public class QueryManager {
 		qexec.close();
 	}
 
-	public static Model runQuery(String queryString, String endpoint) {
+	public static Model runConstructQuery(String queryString, String endpoint) {
 		Query query = QueryFactory.create(queryString);
-		System.out.println(query);
+//		System.out.println(query);
+//		@SuppressWarnings("resource")
+//		QueryEngineHTTP engineHTTP = new QueryEngineHTTP(endpoint, query, (HttpAuthenticator) null);
+//		engineHTTP.setModelContentType(RDFLanguages.strLangRDFXML);
+//		Model model = engineHTTP.execConstruct();
 		QueryExecution qe = QueryExecutionFactory.sparqlService(endpoint, query);
-		// ResultSet resultSet = qe.execSelect();
-		// ResultSetFormatter.out(resultSet);
-		return qe.execConstruct();
+		Model model = qe.execConstruct();
+//		qe.close();
+		return model;
+	}
+
+	public static ResultSet runSelectQueryOnModel(String queryString, Model model) {
+		Query query = QueryFactory.create(queryString);
+		QueryExecution qe = QueryExecutionFactory.create(query, model);
+		ResultSet resultSet = qe.execSelect();
+//		qe.close();
+		return resultSet;
 	}
 
 }
